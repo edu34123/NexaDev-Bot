c34615e1cb79a8ddd7480bbd55cfb24d87fe8499
edu34123 <eduard.puiu34@gmail.com>
2025-10-03 07:24:04 +0200

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    GUILD_ID = int(os.getenv('1423395139258417286'))
+    VERIFY_CHANNEL_ID = int(os.getenv('1423540608508755978'))
+    TICKET_CHANNEL_ID = int(os.getenv('1423395942094344223'))
+    STATUS_CHANNEL_ID = int(os.getenv('1423395930811535444'))
+    STAFF_ROLE_ID = int(os.getenv('1423395883214700594'))
